ccfea020b3e0804638f87c36c6a5e19a12ce86e7
Marco don Kaulea <DonMarco42@gmail.com>
2013-01-08 17:21:25 +0100

    Adds License Notice

--- image2Hacklace.py
+++ image2Hacklace.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# "THE SCOTCH-WARE LICENSE" (Revision 42):
+# <DonMarco42@gmail.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a scotch whisky in return
+# Marco 'don' Kaulea
+# ----------------------------------------------------------------------------
+
+
 import Image
 import sys
 
